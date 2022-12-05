5c8f6e52113cd941dcf6836f98e35344f07b144b
ItzNico <matas.venslauskas.mv@gmail.com>
2022-12-05 21:41:44 +0200

    lecture done

--- JS/INTRO/script.js
+++ JS/INTRO/script.js
@@ -36,7 +36,7 @@ const workplace = 'codeacademy';
 console.log(workplace);
 const group = "CAFE04";
 console.log(group);
-const totalname = 'Total name is : hello';
+const totalname = `Total name is : ${workplace} ${group}`;
 console.log(totalname);
 
 //number
@@ -81,21 +81,39 @@ console.log(typeof colors);
 // let promptAge = prompt("What is your age?");
 // console.log(promptAge);
 
+let x = 50;
+let y = 10;
+let sum = x + y;
+let subtraction = x - y;
+let multiply = x * y;
+let division = x / y;
+console.log(sum);
+console.log(subtraction);
+console.log(multiply);
+console.log(division);
 
-$("#dialogBtn").on("click", dialog.bind(this, 'Are you Sure?', yesCallback, noCallback));
 
-function dialog(message, yesCallback, noCallback) {
-      var modal = $("#modal_dialog");
-      modal.show();
-      $('.title').html(message);
-            
-      $('#dialogBtnYes').off("click").on("click", function() {
-           modal.hide()
-           yesCallback.call();
-      });
-      $('#dialogBtnNo').off("click").on("click", function() {
-            modal.hide()
-            noCallback.call();
-      });
-}
+let num1 = 1;
+let num2 = 2;
+let string1 = "Hello";
+let string2 = "World!"
 
+// document.querySelector('body').innerText = '<a href="#">string1</a>'
+// document.getElementsByTagName("body")[0].innerHTML = string1
+// document.querySelector('body').innerHTML = '<a href="#">string1</a>'
+
+let name;
+let admin;
+admin = "John";
+name = admin;
+document.write(`admin`);
+
+let birthDay = "July 19th";
+let Name = prompt ();
+console.log(Name + " is born on " + birthDay);
+
+console.log("Hello world!");
+alert("I love JavaScript");
+confirm("Ready to learn more?");
+let randomNumber = prompt("Fill in a random number from 1 to 10");
+console.log(randomNumber);
