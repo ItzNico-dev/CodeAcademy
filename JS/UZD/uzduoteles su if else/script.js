05bd1d871e13d3bf421dcea151ccf6da379d70a2
ItzNico <matas.venslauskas.mv@gmail.com>
2022-12-06 20:32:10 +0200

    first question complete

---
+++ JS/UZD/uzduoteles su if else/script.js
@@ -0,0 +1,63 @@
+//? uzduotele 3
+
+
+// const a = prompt("Enter a number");
+
+//!if else
+// if (a % 2 == 0) {
+//     console.log("Even")
+// } else {
+//     console.log("Odd")
+// }
+
+//! ternary
+// a % 2 == 0 ? console.log("Even") : console.log("Odd")
+
+//!switch
+
+// const b = a % 2;
+
+// switch (b) {
+//     case 0: console.log("Even")
+//         break;
+//     default:
+//         console.log("Odd")
+// }
+
+//? uzduotele4
+
+//! if else
+
+// const a = prompt("Enter a number");
+
+// if (a == 0) {
+//     console.log("Not a group")
+// } else if (a == 1) {
+//     console.log("Solo")
+// } else if (a == 2) {
+//     console.log("Duo")
+// } else if (a == 3) {
+//     console.log("Trio")
+// } else if (a == 4) {
+//     console.log("Quartet")
+// } else if (a > 4) {
+//     console.log("Big group")
+// }
+
+//! switch
+
+const a = prompt("Enter a number");
+
+
+switch (a) {
+    case 0: console.log("Not a group");
+        break;
+    case 1: console.log("Solo");
+        break;
+    case 2: console.log("Duo");
+        break;
+    case 3: console.log("Trio");
+        break;
+    case 4: console.log("Quartet");
+        break;
+}
