0a090bc8ed6cf6801c09e9e910946525ce5417f6
ItzNico <matas.venslauskas.mv@gmail.com>
2022-12-07 21:26:58 +0200

    done

---
+++ JS/UZD/loops/script.js
@@ -0,0 +1,44 @@
+//! task 1
+
+// const legalAge = 20;
+
+// let clientAge = prompt("What's your age?")
+// if (clientAge >= legalAge) {
+//     alert("You are over legal age!")
+// } else {
+//     alert("You are under legal age!")
+// }
+
+
+//! task 2
+// let myName = "Matthew";
+
+// if (myName.length >= 6 ) {
+//     console.log( "Your name is long") } else {
+//         console.log("Your name is short")
+//     }
+
+//! task 3
+
+// const myAge = 20;
+
+// if (0 < myAge && myAge < 18) {
+//     console.log("Child");
+// }
+// else if (100 > myAge && myAge >= 18) {
+//     console.log("Adult");
+// } else {
+//     console.log("Age Invalid")
+// }
+
+//! question 4
+
+let car = prompt("Enter your car manufacturer");
+
+if (car == "VW" || car == "Audi" || car == "Bentley" || car == "Bugatti" || car == "Lambo" || car == "Porsche") {
+    console.log("Your car belongs to VW club");
+} else if (car == "BMW" || car == "Mini" || car == "Rolls-Royce") {
+    console.log("Your car belongs to BMW club");
+} else {
+    console.log("Your car belongs nether club");
+}
