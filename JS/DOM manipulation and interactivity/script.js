82842f307df265baa8a9686fe3db0a97ff2a2773
ItzNico <matas.venslauskas.mv@gmail.com>
2022-12-08 19:54:36 +0200

    done

--- JS/DOM manipulation and interactivity/script.js
+++ JS/DOM manipulation and interactivity/script.js
@@ -2,23 +2,40 @@
 
 // console.log(document.getElementsByTagName('div'));
 // console.log(document.getElementsByClassName('second'));
-// console.log(document.getElementsByName('third'));
 //  console.log(document.getElementById('third'));
 
-console.log(document.querySelector('#first'))
-console.log(document.querySelector('.second'))
-console.log(document.querySelector('div'))
-console.log(document.querySelectorAll('div'))
+// console.log(document.querySelector('#first'))
+// console.log(document.querySelector('.second'))
+// console.log(document.querySelector('div'))
+// console.log(document.querySelectorAll('div'))
 
 
-const myDiv = document.querySelector('div')
+// const myDiv = document.querySelector('div')
 
-console.log(myDiv.classList)
-myDiv.classList.remove('background')//removes class
-myDiv.classList.add('background')//adds class
-myDiv.classList.toggle('background') //removes if there/adds if not there
+// console.log(myDiv.classList)
+// myDiv.classList.remove('background')//removes class
+// myDiv.classList.add('background')//adds class
+// myDiv.classList.toggle('background') //removes if there/adds if not there
 
-console.log(myDiv.classList.contains('background')) // checks oif such a class is there
+// console.log(myDiv.classList.contains('background')) // checks oif such a class is there
 
-myDiv.style.backgroundColor = 'red'//can target individual style properties this way
+// myDiv.style.backgroundColor = 'red'//can target individual style properties this way
 
+// console.log(myDiv.textContent)
+// myDiv.textContent = 'Halo'
+
+// console.log(myDiv.innerHTML)
+
+// const myDivs = document.querySelectorAll('div')
+// console.log(myDivs)
+
+// for (i = 0; i < myDivs.length; i++) {
+//     console.log('Hello')
+// }
+
+console.log(document.getElementById('age-table'));
+console.log(document.getElementById('age'));
+console.log(document.querySelectorAll('label'));
+console.log(document.querySelector('input'));
+
+console.log(document.querySelector('tr:last-child td:last-child input'));
