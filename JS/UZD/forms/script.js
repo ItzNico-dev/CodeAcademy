4bc689cfe1ce66a9dad1d5386d06ab45cac5d792
ItzNico <matas.venslauskas.mv@gmail.com>
2022-12-14 23:41:02 +0200

    tired evening

---
+++ JS/UZD/forms/script.js
@@ -0,0 +1,15 @@
+// Sukurkite kino teatro kainos apskaičiavimo platformą.
+// Joje turi būti vienas input laukelis, kur žmogus įrašo savo
+// amžių, o JS apskaičiuoja kainą ir išmeta ją h1 elemente.Standartinis
+// bilietas – 6eu, iki 16 metų – 50proc nuolaida, 1 / 3 nuolaida vyresniems
+//  nei 60. Variable turi būti aprašyti viršuj, kad būtų lengva keist.
+
+
+
+// Kai kuriose šalyse reikia eiti į kariuomenę, jei tu tarp 18 ir 30,
+// tačiau, jei neturi kriminalinio įrašo.Sukurk programą, kuri su
+// input + radio button pasakytų ar tam žmogui gali reikėti eiti į
+// kariuomenę(h1 elemente).Hint: pasirinkite su querySelector tik pažymėtą
+// radio input(aha, yra toks CSS selektorius), ir perskaitykite jo reikšmę.
+
+
