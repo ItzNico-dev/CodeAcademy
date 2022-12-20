f92d504f052bdcac2e2eeb8db14ed4b148336cac
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2022-12-20 20:32:31 +0200

    test

--- JS/recap/script.js
+++ JS/recap/script.js
@@ -107,3 +107,113 @@ const obj = {
 //! ternary (?)
 
 // 5 > 10 ? console.log('teisybe'): console.log('neteisybe')
+
+//! more
+
+// console.log()
+// console.dir()
+// alert()
+// prompt()
+// confirm()
+// Math. // floor min max round
+// number. // parseInt same as a (+) before 
+// typeof // says the type of data
+// new Date() //getTime, getFullYear, getMonth etc...
+
+//! cycles
+
+// for(let index = 0; index < 10; index++){
+//     console.log(index)
+// }
+
+// let index = 0
+// while(index < 10){
+//     console.log(index)
+//     index++
+// }
+
+// let index = 0
+// do{
+//     console.log(index)
+//     index++
+// } while(index > 10)
+
+//! selectors
+
+// const item1 = document.getElementById('#elementID')
+// const item2 = document.querySelector('div') // anything
+// const item2 = document.querySelector('.div') //class
+// const item2 = document.querySelector('#div') //ID
+// const item3 = document.querySelectorAll('div')
+
+//! events
+
+// const body = document.querySelector('body')
+// body.addEventListener('click', myFunction)
+// function myFunction(){}... 
+
+// body.addEventListener('click', () => {
+//     console.log('say hello')
+// })
+
+// document.querySelector('input').addEventListener('focus', () => {
+//     console.log('focus')
+// })
+
+// document.querySelector('input').addEventListener('input', (e) => {
+//     console.log(e.target.value)
+// })
+
+//! functions
+
+// function myFunction(name, age){
+//     console.log(name,age)
+// }
+
+// myFunction('Matt', 20)
+
+//!------------------------------------------------------------
+
+// const myFunction = function(){
+//     console.log('sadasdas')
+// }
+
+//!------------------------------------------------------------
+
+// const arrowFunction = (name, age) => {
+//     console.log(name, age)
+// }
+// arrowFunction('asdas', 198)
+
+//!------------------------------------------------------------
+
+// const arrowFunction = (name, age) => console.log(name, age)
+
+// arrowFunction('asdas', 198)
+
+//!------------------------------------------------------------
+
+// const arrowFunction = name => console.log(name)
+
+// arrowFunction('asdasd')
+
+//! intervals
+
+// setInterval(() => {
+//     console.log('hello')
+// }, 1000);
+
+//! timeout
+
+// setTimeout(() => {
+//     console.log('hello')
+// }, 1000);
+
+//! document location
+
+// console.log(document.location)
+// document.location.assign('https://google.com/')
+// document.location.assign('second.html')
+// document.location.reload()
+// console.log(document.body)
+// console.log(document.forms)
