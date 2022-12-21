acf9410567f109b98f12a2e89fdd88d459193795
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2022-12-21 20:08:40 +0200

    done

--- JS/UZD/Objects/script.js
+++ JS/UZD/Objects/script.js
@@ -4,13 +4,19 @@
 //     brand: 'BMW'
 
 // }
-// // console.log(car)
 // console.log(car.doors)
 // console.log(car.color)
 // console.log(car.brand)
 
 
-document.querySelector('form').addEventListener('submit', (e) => (
-    e.preventDefault
-    const name = e.target
-))
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+ 
+    const person = {
+        name: document.querySelector('input').value,
+        surname: document.querySelector('input:nth-of-type(2)').value,
+        age: document.querySelector('input:nth-of-type(3)').value
+    }
+
+    console.log(person)
+})
