fc5d1a3c8ec1cf593e2ef87b90945c34ebcfce3d
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2022-12-29 18:57:56 +0200

    task 1 n 2 done

--- JS/recap/script.js
+++ JS/recap/script.js
@@ -607,3 +607,91 @@ const sayHelloArrow = () => {
 
 //! html element creation JS
 
+const myBody = document.querySelector('body')
+const myMain = document.querySelector('main')
+// const test = document.createElement('p')
+// test.textContent = 'hello world'
+// myBody.append(test) //!add as a first element = prepend
+
+
+//! task 1
+
+// const data = [
+//    "Apple",
+//   "Banana",
+//   "Orange",
+//   "Grape",
+//   "Peach",
+//   "Mango"
+// ]
+
+// const ulList = document.createElement('ul')
+
+
+// for (let index = 0; index < data.length; index++) {
+//   const liElement = document.createElement('li')
+//   liElement.textContent = data[index]
+//   ulList.append(liElement)
+// }
+
+// myBody.append(ulList)
+
+//!task 2
+
+
+const data = [
+
+  { name: "John", age: 30, country: "USA" },
+  { name: "Jane", age: 25, country: "Canada" },
+  { name: "Bob", age: 35, country: "Mexico" },
+  { name: "Alice", age: 28, country: "USA" },
+  { name: "Tom", age: 32, country: "Canada" },
+  { name: "Sue", age: 27, country: "Mexico" }
+
+];
+
+const ulElement = document.createElement('ul')
+const nameInput = document.querySelector('input[name="name"]')
+const ageInput = document.querySelector('input[name="age"]')
+const countryInput = document.querySelector('input[name="country"]')
+
+document.querySelector('#generate-button').addEventListener('click', generateTable)
+document.querySelector('#add-button').addEventListener('click', addItem)
+const personTable = document.querySelector('table')
+
+function addItem(){
+  const name = nameInput.value
+  const age = ageInput.value
+  const country = countryInput.value
+
+  const person = {
+    name,
+    age,
+    country,
+  }
+  personTable.append(generateTableRow(person))
+}
+
+function generateTable(){
+  
+  for (let index = 0; index < data.length; index++) {
+    personTable.append(generateTableRow(data[index]))
+  }
+
+}
+
+function generateTableRow(personData){
+  const trElement = document.createElement('tr')
+
+    const tdName = document.createElement('td')
+    tdName.textContent = personData.name
+    const tdAge = document.createElement('td')
+    tdAge.textContent = personData.age
+    const tdCountry = document.createElement('td')
+    tdCountry.textContent = personData.country
+
+    trElement.append(tdName, tdAge, tdCountry)
+
+    return trElement
+
+}
