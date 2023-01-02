3fd494f818bc61075d77b6dd74ffb23eaadbc4df
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-02 21:41:16 +0200

    done for the day

--- JS/UZD/Arrays/script.js
+++ JS/UZD/Arrays/script.js
@@ -1,22 +1,37 @@
- const randomArr = [
-    11,
-    'cat',
-    true,
-    4,
-    'mouse',
-    5,
-    9,
-    5,
-    76,
-    false
- ]
- function getArrayByData(array, dataType){
-    if( typeof randomArr === 'string'){
-        for (let i = 0; i < randomArr.length; i++) {
-            console.log(randomArr[i])
-        } else if( typeof randomArr === 'number'){
-            console.log()
-        }
-    }
-    
- }
+//! class task
+
+//  const randomArr = [
+//     11,
+//     'cat',
+//     true,
+//     4,
+//     'mouse',
+//     5,
+//     9,
+//     5,
+//     76,
+//     false
+//  ]
+//  function getArrayByData(array, dataType){
+//     let result = []
+//     for (let i = 0; i < array.length; i++) {
+//         if (typeof array[i] === dataType) {
+//             result.push(array[i])
+//         }
+        
+//     }
+//     return result
+//  }
+
+//  console.log(getArrayByData(randomArr, 'boolean'))
+
+//! task 1 & 2
+
+const cars = [
+    'BMW',
+    'VW',
+    'Audi'
+]
+cars.forEach(element => {
+    console.log(element)
+})
