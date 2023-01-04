e064dc2c4780ffeb793de90a7d22ecf2ea8046cc
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-04 21:09:14 +0200

    progress

---
+++ JS/UZD/Array practice/script.js
@@ -0,0 +1,68 @@
+//! page 1 task 1
+
+// function minutesToSecconds(number){
+//     return number * 60
+// }
+
+// console.log(minutesToSecconds(60))
+
+//! page 1 task 2
+
+// function ageInDays(age){
+//     return age *365
+// }
+
+// console.log(ageInDays(20))
+
+//! page 1 task 3
+
+// const squareNum = (x) => x * x
+
+// console.log(squareNum(5))
+
+//! page 1 task 4
+
+// const triangleArea = (a,b) => (a*b)/2
+// console.log(triangleArea(6,6))
+
+//! page 1 task 5
+
+
+
+//! page 1 task 6
+
+// const numbers =  [-1, -100, -5, 10, 0, 11] 
+
+// function biggestNegativeNum(num){
+//     num.sort((a,b) => b-a )
+// }
+
+// biggestNegativeNum(numbers)
+// console.log(numbers[5])
+//! page 1 task 7
+
+
+
+//! page 1 task 8
+
+
+
+//! page 1 task 9
+
+
+
+//! page 1 task 10
+
+
+
+//! page 1 task 11
+
+
+
+//! page 1 task 12
+
+
+
+
+
+
