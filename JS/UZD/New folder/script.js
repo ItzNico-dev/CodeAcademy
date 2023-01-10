b5243b06536280e5e86c8d15f5e1bb7919856355
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-10 22:01:52 +0200

    initial

---
+++ JS/UZD/New folder/script.js
@@ -0,0 +1,13 @@
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+		// Rašyti kodą čia
+	}
+}
+
+const p1 = new Person("Petras", 16)
+p1.compareAge() => "Petras is old enough to drink";
