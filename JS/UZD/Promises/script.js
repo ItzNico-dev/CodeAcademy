50221d5a9e7cd8b943fb91cb07c8e3da31641553
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-16 20:34:06 +0200

    elcture done. tasks started

--- JS/UZD/Promises/script.js
+++ JS/UZD/Promises/script.js
@@ -154,3 +154,55 @@
 //! promises with async/await
 
 //?rewrite 
+
+// fetch(apiUrl)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((e) => console.log("Error got in fetching post: " + e))
+//   .finally(() => console.log("Fetching finished"));
+
+// const asyncFunction = async () => {
+//     const response = fetch(apiUrl)
+//     const data = await response.json()
+//     console.log(data)
+// }
+
+// asyncFunction()
+
+const promise2 = new Promise((resolve) => {
+  setTimeout(resolve, 500, "start");
+});
+
+const promise3 = new Promise((resolve) => {
+  setTimeout(resolve, 1500, "second promise fulfilled");
+});
+
+const promise4 = new Promise((resolve) => {
+  setTimeout(resolve, 300, "last promise fulfilled");
+});
+
+
+
+Promise.all([promise2, promise3, promise4]).then((data) => console.log(data));
+
+async function waitForAll() {
+    const data = await Promise.all((promise2, promise3, promise4))
+    console.log(data)
+}
+
+// waitForAll()
+
+//? errors with async/await
+
+const catchError = async () => {
+  try {
+    const resultPromiseTwo = await promise2;
+    console.log(resultPromiseTwo);
+  } catch (e) {
+    console.log("Error caught: " + e);
+  } finally {
+    console.log('finished working with promise')
+  }
+};
+
+catchError();
