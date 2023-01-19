a45e7b74a05bb21009d06fa9aa4cf988ec4a2dd0
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-19 18:07:25 +0200

    initial

---
+++ JS/GetPost practice/script.js
@@ -0,0 +1,50 @@
+//? Create a simple to-do list application using JavaScript 
+//? and local storage.
+
+
+
+
+
+//? The application should have the following features:
+
+
+
+
+//? 1. A form for adding new to-do items to the list.
+
+
+
+
+//? 2. A display area for showing all the items on the list.
+
+
+
+
+
+//? 3. A button for clearing the entire list.
+
+
+
+
+
+//? 4. Use local storage to save the to-do list so that it persists 
+//? even when the user closes the browser or refreshes the page.
+
+
+
+
+//? Instructions:
+
+//? 1. Create an HTML file with a form for adding new to-do items.
+//? The form should have a text input and a button for submitting the item.
+
+//? 2. Create a JavaScript file to handle the logic for the to-do list application.
+
+//? 3. Use the localStorage object to save the to-do list to the user's browser.
+
+//? 4. When the page loads, check if there is a saved to-do list in local storage
+//? and display it if there is.
+
+//? 5. When the user submits a new item, add it to the list and update the local storage.
+
+//? 6. Add a button that allows the user to clear the entire list and clears the local storage.
