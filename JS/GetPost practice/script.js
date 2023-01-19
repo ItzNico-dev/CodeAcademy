46232228c788d00bf12bcce230af922ea8c6f1f1
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-19 18:56:18 +0200

    .

--- JS/GetPost practice/script.js
+++ JS/GetPost practice/script.js
@@ -1,15 +1,15 @@
-const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
-const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts';
-const COMMENTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/comments';
+// const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+// const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts';
+// const COMMENTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/comments';
 
-const mainElement = document.querySelector('main');
+// const mainElement = document.querySelector('main');
 
-async function makeRequest(endpoint) {
-  const res = await fetch(endpoint);
-  const data = await res.json();
+// async function makeRequest(endpoint) {
+//   const res = await fetch(endpoint);
+//   const data = await res.json();
   
-  return data;
-}
+//   return data;
+// }
 
 // {
 //   "id": 1,
@@ -33,37 +33,58 @@ async function makeRequest(endpoint) {
 //   "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
 // }
 
-async function generateHtml(){
-  const [users, posts, comments] = await Promise.all([
-    makeRequest(USERS_ENDPOINT),
-    makeRequest(POSTS_ENDPOINT),
-    makeRequest(COMMENTS_ENDPOINT)
-  ])
+// async function generateHtml(){
+//   const [users, posts, comments] = await Promise.all([
+//     makeRequest(USERS_ENDPOINT),
+//     makeRequest(POSTS_ENDPOINT),
+//     makeRequest(COMMENTS_ENDPOINT)
+//   ])
 
-  users.forEach(user => {
-    const containerDiv = document.createElement('div');
-    const userEmailH1 = document.createElement('h1');
-    userEmailH1.textContent = user.email;
-    containerDiv.append(userEmailH1);
+//   users.forEach(user => {
+//     const containerDiv = document.createElement('div');
+//     const userEmailH1 = document.createElement('h1');
+//     userEmailH1.textContent = user.email;
+//     containerDiv.append(userEmailH1);
 
-    const usersPosts = posts.filter(post => post.userId === user.id);
-    usersPosts.forEach(post => {
-      const postsComments = comments.filter(comment => comment.postId === post.id);
-      const postElement = document.createElement('p');
-      postElement.textContent = post.body;
-      postElement.style.color = 'blue'
-      containerDiv.append(postElement);
+//     const usersPosts = posts.filter(post => post.userId === user.id);
+//     usersPosts.forEach(post => {
+//       const postsComments = comments.filter(comment => comment.postId === post.id);
+//       const postElement = document.createElement('p');
+//       postElement.textContent = post.body;
+//       postElement.style.color = 'blue'
+//       containerDiv.append(postElement);
 
-      postsComments.forEach(comment => {
-        const commentElement = document.createElement('p');
-        commentElement.textContent = comment.body;
-        containerDiv.append(commentElement);
-      })
-    })
-    const hrElement = document.createElement('hr');
-    containerDiv.append(hrElement);
-    mainElement.append(containerDiv)
-  });
-}
+//       postsComments.forEach(comment => {
+//         const commentElement = document.createElement('p');
+//         commentElement.textContent = comment.body;
+//         containerDiv.append(commentElement);
+//       })
+//     })
+//     const hrElement = document.createElement('hr');
+//     containerDiv.append(hrElement);
+//     mainElement.append(containerDiv)
+//   });
+// }
 
-generateHtml()
+// generateHtml()
+
+//! -------------------------------------------------------------------------
+
+
+// async function makeRequest(){
+//     const ENDPOINT = 'https://www.omdbapi.com/?apikey=d1311396&i=tt1285056'
+//     const res = await fetch(ENDPOINT)
+//     const data = await res.json()
+//     console.log(data)
+// }
+
+makeRequest()
+
+
+
+
+
+
+
+
+
