5f49d39e12c6ba77e3e672ee7082c0e8214c0897
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-19 18:42:09 +0200

    done

--- JS/GetPost practice/script.js
+++ JS/GetPost practice/script.js
@@ -1,107 +1,69 @@
-//? Create a simple to-do list application using JavaScript 
-//? and local storage.
-//? The application should have the following features:
+const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts';
+const COMMENTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/comments';
 
+const mainElement = document.querySelector('main');
 
-
-
-//? 1. A form for adding new to-do items to the list.
-
-
-
-
-//? 2. A display area for showing all the items on the list.
-
-
-
-
-
-//? 3. A button for clearing the entire list.
-
-
-
-
-
-//? 4. Use local storage to save the to-do list so that it persists 
-//? even when the user closes the browser or refreshes the page.
-
-
-
-
-//? Instructions:
-
-//? 1. Create an HTML file with a form for adding new to-do items.
-//? The form should have a text input and a button for submitting the item.
-
-//? 2. Create a JavaScript file to handle the logic for the to-do list application.
-
-//? 3. Use the localStorage object to save the to-do list to the user's browser.
-
-//? 4. When the page loads, check if there is a saved to-do list in local storage
-//? and display it if there is.
-
-//? 5. When the user submits a new item, add it to the list and update the local storage.
-
-//? 6. Add a button that allows the user to clear the entire list and clears the local storage.
-
-const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users'
-const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
-const COMMENTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/comments'
-
-
-async function getUsers(){
-    USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users'
-    const res = await fetch(USERS_ENDPOINT)
-    const users = await res.json()
-    return data
+async function makeRequest(endpoint) {
+  const res = await fetch(endpoint);
+  const data = await res.json();
+  
+  return data;
 }
 
+// {
+//   "id": 1,
+//   "name": "Leanne Graham",
+//   "username": "Bret",
+//   "email": "Sincere@april.biz",
+// }
 
+// {
+//   "userId": 1,
+//   "id": 1,
+//   "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//   "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+// }
 
+// {
+//   "postId": 1,
+//   "id": 1,
+//   "name": "id labore ex et quam laborum",
+//   "email": "Eliseo@gardner.biz",
+//   "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+// }
 
+async function generateHtml(){
+  const [users, posts, comments] = await Promise.all([
+    makeRequest(USERS_ENDPOINT),
+    makeRequest(POSTS_ENDPOINT),
+    makeRequest(COMMENTS_ENDPOINT)
+  ])
 
-async function getPosts(){
-    POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
-    const res = await fetch(POSTS_ENDPOINT)
-    const posts = await res.json()
-    console.log(posts)
+  users.forEach(user => {
+    const containerDiv = document.createElement('div');
+    const userEmailH1 = document.createElement('h1');
+    userEmailH1.textContent = user.email;
+    containerDiv.append(userEmailH1);
+
+    const usersPosts = posts.filter(post => post.userId === user.id);
+    usersPosts.forEach(post => {
+      const postsComments = comments.filter(comment => comment.postId === post.id);
+      const postElement = document.createElement('p');
+      postElement.textContent = post.body;
+      postElement.style.color = 'blue'
+      containerDiv.append(postElement);
+
+      postsComments.forEach(comment => {
+        const commentElement = document.createElement('p');
+        commentElement.textContent = comment.body;
+        containerDiv.append(commentElement);
+      })
+    })
+    const hrElement = document.createElement('hr');
+    containerDiv.append(hrElement);
+    mainElement.append(containerDiv)
+  });
 }
 
-
-
-
-
-async function makeRequest(endpoint){
-    const res = await fetch(endpoint)
-    const data = await res.json()
-    return data
-}
-
-async function generateHTML(){
-    const [users, posts, comments] = await Promise.all([
-        makeRequest(USERS_ENDPOINT),
-        makeRequest(POSTS_ENDPOINT),
-        makeRequest(COMMENTS_ENDPOINT)
-    ])
-
-
-users.forEach(user => {
-
-    const containerDiv = document.createElement('div')
-    userEmailH1 = document.createElement('h1')
-    userEmailH1.textContent = user.email
-    containerDiv.append(userEmailH1)
-
-    const usersPosts = posts.filter(post => post.userId === user.id)
-    usersPosts.forEach(post => (
-        const postsCmments = comments.filter(comment => comment.postId === post.id)
-        const postElement  = document.createElement('p')
-        postElement.textContent = post.body
-        containerDiv.append(postElement)
-    
-        ))
-
-
-
-});
-}
+generateHtml()
