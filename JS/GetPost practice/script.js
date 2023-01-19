8cc315dec871eedce883a0a5a954c697557a26d6
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-19 18:40:33 +0200

    sort of done

--- JS/GetPost practice/script.js
+++ JS/GetPost practice/script.js
@@ -1,10 +1,5 @@
 //? Create a simple to-do list application using JavaScript 
 //? and local storage.
-
-
-
-
-
 //? The application should have the following features:
 
 
@@ -48,3 +43,65 @@
 //? 5. When the user submits a new item, add it to the list and update the local storage.
 
 //? 6. Add a button that allows the user to clear the entire list and clears the local storage.
+
+const USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users'
+const POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
+const COMMENTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/comments'
+
+
+async function getUsers(){
+    USERS_ENDPOINT = 'https://jsonplaceholder.typicode.com/users'
+    const res = await fetch(USERS_ENDPOINT)
+    const users = await res.json()
+    return data
+}
+
+
+
+
+
+async function getPosts(){
+    POSTS_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
+    const res = await fetch(POSTS_ENDPOINT)
+    const posts = await res.json()
+    console.log(posts)
+}
+
+
+
+
+
+async function makeRequest(endpoint){
+    const res = await fetch(endpoint)
+    const data = await res.json()
+    return data
+}
+
+async function generateHTML(){
+    const [users, posts, comments] = await Promise.all([
+        makeRequest(USERS_ENDPOINT),
+        makeRequest(POSTS_ENDPOINT),
+        makeRequest(COMMENTS_ENDPOINT)
+    ])
+
+
+users.forEach(user => {
+
+    const containerDiv = document.createElement('div')
+    userEmailH1 = document.createElement('h1')
+    userEmailH1.textContent = user.email
+    containerDiv.append(userEmailH1)
+
+    const usersPosts = posts.filter(post => post.userId === user.id)
+    usersPosts.forEach(post => (
+        const postsCmments = comments.filter(comment => comment.postId === post.id)
+        const postElement  = document.createElement('p')
+        postElement.textContent = post.body
+        containerDiv.append(postElement)
+    
+        ))
+
+
+
+});
+}
