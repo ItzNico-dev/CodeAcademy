62176ab85b2e753c293e5a61e0bc86a3d1b357e9
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-01-23 22:47:18 +0200

    done for today

--- JS/GetPost practice/script.js
+++ JS/GetPost practice/script.js
@@ -96,3 +96,43 @@
 // )
 // .then(() => alert('works')).catch(() => alert('Oops, promise was rejected'))
 
+// const renderUserCard = (user) => {
+//   const img = document.createElement('img');
+//   img.src = user.picture.large;
+//   img.alt = `${user.name.first} profile picture`;
+
+//   const intro = document.createElement('h4');
+//   intro.innerText = `${user.name.first} ${user.name.last}, ${user.dob.age}`;
+
+//   const contacts = document.createElement('h5');
+//   contacts.innerText = user.email;
+
+//   const card = document.createElement('div');
+//   card.append(img, intro, contacts);
+//   document.body.append(card);
+// };
+
+// const fetchRandomUser = async () => {
+//   try {
+//     const response = await fetch('https://randomuser.me/api/');
+//     if (response.ok) {
+//       const data = await response.json();
+//       renderUserCard(data.results[0]);
+//     }
+//   } catch(error) {
+//     console.error(error);
+//   }
+// };
+
+// fetchRandomUser();
+
+
+const taskURL = "https://boiling-reaches-93648.herokuapp.com/week-3/party"
+
+const checkData = async () => {
+    const response = await fetch(taskURL)
+    const data = response.json()
+    console.log(data)
+}
+
+checkData()
