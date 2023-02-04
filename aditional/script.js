b76cb8a05016da37a8c1e32ada904bf50f29a8c9
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-02-05 00:58:56 +0200

    practice

---
+++ aditional/script.js
@@ -0,0 +1,29 @@
+// const legalAge = 20
+// const clientAge = 15
+// if (legalAge > clientAge) {
+//     console.log('underage')
+// } else {
+//     console.log('legal age')
+// }
+
+
+// myName = 'Matt'
+
+// if (myName.length > 3) {
+//     console.log('long')
+// } else {
+//     console.log('short')
+// }
+
+
+// clientAge = 0
+
+// if (clientAge < 1 || clientAge > 100) {
+//     console.log('invalid age')
+// } else if(clientAge >= 1 && clientAge <= 18){
+//     console.log('not an adult')
+// } else{
+//     console.log('adult')
+// }
+
+
