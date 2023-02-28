b72ed351ce8746f0d460c55daa28425f85df5e62
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-02-28 21:13:01 +0200

    made some changes and progress

--- Node JS/SAvarankiskasMySQL/backend/server.js
+++ Node JS/SAvarankiskasMySQL/backend/server.js
@@ -1,2 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
