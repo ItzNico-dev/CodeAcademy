f732b34f150ab4891204692f0c9472e4c9c18404
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-03-13 19:43:41 +0200

    .

--- Node JS/NodeJS Egz/routes/router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getUsersNames } from '../controllers/controllers.js'
-const router = express.Router()
-
-router.get('https//jsonplaceholder.typicode.com/users/names', getUsersNames);
-
-
-export default router
