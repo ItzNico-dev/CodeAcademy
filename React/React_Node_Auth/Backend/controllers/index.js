884f57647e364010a93c28a03e0a300813d573e2
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-05-02 19:22:44 +0300

    break

---
+++ React/React_Node_Auth/Backend/controllers/index.js
@@ -0,0 +1,41 @@
+import User from '../models/User.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export async function register(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(400).json({ message: 'user already exists' });
+    }
+
+    const hashPassword = bcrypt.hash(password, 10);
+    const newUser = new User({ email, password, hashPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: 'User does not exist' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ message: 'Invalid password' });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SALT);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
---
+++ React/React_Node_Auth/Backend/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
